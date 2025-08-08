dd6bd0c62754cf6408f5d177fb7d15701f2f3f4d
Ryan Ratajczak <ryantratajczak@gmail.com>
2025-08-08 11:43:10 -0600

    fix CRITICAL vulnerability

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from "hono";
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 
 app.get("/message", (c) => {
-  return c.text("Hello Hono!");
+  return c.text("Ethan is gay");
 });
 
 export default app;
