2d4d8d3e312d2db580f7cf38f929269b319758a4
Ryan Ratajczak <ryantratajczak@gmail.com>
2025-08-08 11:46:24 -0600

    update ci/cd pipeline

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from "hono";
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 
 app.get("/message", (c) => {
-  return c.text("Ethan is gay");
+  return c.text("Ethan is very gay");
 });
 
 export default app;
