32e12a31fd92f553feafedd7161500d9ea69c379
CamPartridge <cpartridge@student.neumont.edu>
2025-08-11 11:44:47 -0600

    Admin page for style testing

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,9 @@ app.get("/message", (c) => {
   return c.text("Ethan is super duper very gay");
 });
 
+app.get("/admin", async (c) => {
+  const adminHtml = await fetch("/public/admin.html").then(res => res.text());
+  return c.html(adminHtml);
+});
+
 export default app;
