05ac74121de573bba591e29a96683165f3e3154b
eaheil <eric.heil@ambientproptech.com>
2025-08-11 11:39:10 -0600

    edited

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from "hono";
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 
 app.get("/message", (c) => {
-  return c.text("Ethan is super duper very exceptionally gay");
+  return c.text("Ethan is super duper extraordinarily very exceptionally gay");
 });
 
 export default app;
