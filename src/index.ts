af2a3cbc9d7a690983af64362013b8b8eefbed6d
Damien Krock <123998300+NaokiKioko@users.noreply.github.com>
2025-08-12 23:02:29 -0600

    Compressed hashing functions

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,10 @@
 import { Hono } from "hono";
-import { compareHash, compareHashWithSalt, hashString, hashStringWithSalt } from "./util/hash";
+import { compareHash, hashString } from "./util/hash";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 
 app.get("/message", (c) => {
-  let salt = "gayEthan"
-  let msg = "Ethan is super duper very exceptionally gay"
-  let msgHash = hashString(msg)
-  let msgHashSalt = hashStringWithSalt(msg, salt)
-
-  return c.text(msg + " = "+ msgHash + " Are they really equal: "+ 
-    (compareHash(msg, msgHash)? "YES" : "NO")+
-    " hashed with salt compare = " + (compareHashWithSalt(msg, salt, msgHashSalt)? "YES" : "NO")
-  );
-  // return c.text("Ethan is super duper very exceptionally gay");
+  return c.text("Ethan is super duper very exceptionally gay");
 });
 
 export default app;
--- src/util/hash.ts
+++ src/util/hash.ts
@@ -1,22 +1,15 @@
-import {Md5} from 'ts-md5';
+import { Md5 } from 'ts-md5';
 
-function hashString(text:string) {
-    return Md5.hashStr(text);
+function hash(text: string, salt?: string): string {
+  const saltedText = salt ? `${salt}${text}${salt}` : text;
+  return Md5.hashStr(saltedText);
 }
 
-function hashStringWithSalt(text:string, salt:string) {
-    return Md5.hashStr(salt+text+salt);
+function compareHash(plain: string, hashed: string, salt?: string): boolean {
+  return hash(plain, salt) === hashed;
 }
 
-function compareHash(plain:string, hashed:string) {
-    let hashedPlain = hashString(plain);
-    return hashed == hashedPlain
-}
-
-function compareHashWithSalt(plain:string, salt:string, hashed:string) {
-    let hashedPlain = hashStringWithSalt(plain, salt);
-    return hashed == hashedPlain
-}
-
-
-export {hashString, hashStringWithSalt, compareHash, compareHashWithSalt}
+export {
+  hash as hashString,             // without salt
+  compareHash,                    // without or with salt
+};
